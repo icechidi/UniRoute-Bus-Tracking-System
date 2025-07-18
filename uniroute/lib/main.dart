571644f74a9fc467e75cc00496bfb6157ecbe3e6
Ice Chidi <icechidi3@gmail.com>
2025-07-18 03:22:28 +0300

    API Keys and Git privacy & protection

--- uniroute/lib/main.dart
+++ uniroute/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:device_preview/device_preview.dart';
 // import 'package:flutter/foundation.dart';
 import 'screens/home_screen.dart';
@@ -12,8 +13,9 @@ import 'screens/home_screen.dart';
 //     ),
 //   );
 // }
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const BusApp());
 }
 
