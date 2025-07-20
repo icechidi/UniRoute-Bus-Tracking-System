5c8f8bb11ef143bb8bd08d29e636e2eae00f92f6
Ice Chidi <icechidi3@gmail.com>
2025-07-20 23:47:39 +0300

    2nd Update before removing the andriod v1 errors

--- uniroute/ios/Runner/AppDelegate.swift
+++ uniroute/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(BuildEnv.googleMapsAPIKey)
+    GMSServices.provideAPIKey(["AIzaSyAMXlmK_FnSVjKr6Aap4x2e4T8mIKv2eJI"])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
